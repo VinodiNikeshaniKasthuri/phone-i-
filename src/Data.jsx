730b9a2eb07849d7b0d5ad576e4f6faee58be404
Vinodi NK <140379202+VinodiNikeshaniKasthuri@users.noreply.github.com>
2024-02-27 00:21:24 +0530

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,109 @@
+const DATA = [
+    {
+        id : 0,
+        title : "IPhone 11",
+        price : 165750,
+        desc : "Display: 6.1-inch Liquid Retina HD.Design: Aluminum and glass.Cameras: Dual 12MP Ultra-Wide and Wide, Night mode.Chip: A13 Bionic.Battery: All-day battery life.Face ID: Enabled by TrueDepth camera.Water Resistance: IP68.iOS: iOS 13 (upgradable).Connectivity: Lightning, Wi-Fi 6, LTE.Extras: FaceTime audio, wireless charging, fast charging support.i need 11 pro detail",
+        img : "/assets/images/products/iphone11.png"
+    },
+    {
+        id : 1,
+        title : "IPhone 11 Pro Max",
+        price : 199800,
+        desc : "Display: 6.5-inch Super Retina XDR.Design: Stainless steel and glass.Cameras: Triple 12MP system, Night mode, 4K HDR.Chip: A13 Bionic.Battery: All-day life, fast and wireless charging.Face ID: Enabled by TrueDepth camera.Water Resistance: IP68.iOS: iOS 13 (upgradable).Connectivity: Lightning, Wi-Fi 6, LTE.Extras: Dual SIM, Stereo speakers, FaceTime audio.",
+        img : "/assets/images/products/iphone11promax.png"
+    },
+    {
+        id : 2,
+        title : "IPhone 12 Mini",
+        price : 211950,
+        desc : "Display: 5.4-inch Super Retina XDR.Design: Aerospace-grade aluminum and Ceramic Shield front cover.Cameras: Dual 12MP Ultra-Wide and Wide, Night mode.Chip: A14 Bionic.Battery: All-day battery life, MagSafe wireless charging.Face ID: Enabled by TrueDepth camera.Water Resistance: IP68.iOS: iOS 14 (upgradable).Connectivity: 5G capable, Lightning, Wi-Fi 6.Extras: Dual SIM, 4K Dolby Vision HDR recording.",
+        img : "/assets/images/products/iphone12mini.png"
+    },
+    {
+        id : 3,
+        title : "IPhone 12",
+        price : 211050,
+        desc : "Display: 6.1-inch Super Retina XDR.Design: Aerospace-grade aluminum and Ceramic Shield front cover.Cameras: Dual 12MP Ultra-Wide and Wide, Night mode.Chip: A14 Bionic.Battery: All-day battery life, MagSafe wireless charging.Face ID: Enabled by TrueDepth camera.Water Resistance: IP68.iOS: iOS 14 (upgradable).Connectivity: 5G capable, Lightning, Wi-Fi 6.Extras: Dual SIM, 4K Dolby Vision HDR recording.",
+        img : "/assets/images/products/iphone12.png"
+    },
+    {
+        id : 4,
+        title : "IPhone 12 Pro",
+        price : 281150,
+        desc : "Display: 6.1-inch Super Retina XDR.Design: Surgical-grade stainless steel, Ceramic Shield front cover.Cameras: Triple 12MP Ultra-Wide, Wide, and Telephoto, Night mode, LiDAR Scanner.Chip: A14 Bionic.Battery: All-day battery life, MagSafe wireless charging.Face ID: Enabled by TrueDepth camera.Water Resistance: IP68.iOS: iOS 14 (upgradable).Connectivity: 5G capable, Lightning, Wi-Fi 6.Extras: Dual SIM, 4K Dolby Vision HDR recording, LiDAR Scanner for AR.",
+        img : "/assets/images/products/iphone12pro.png"
+    },
+    {
+        id : 5,
+        title : "IPhone 12 Pro Max",
+        price : 271250,
+        desc : "Display: 6.7-inch Super Retina XDR.Design: Surgical-grade stainless steel, Ceramic Shield front cover.Cameras: Triple 12MP Ultra-Wide, Wide, and Telephoto, Night mode, LiDAR Scanner.Chip: A14 Bionic.Battery: All-day battery life, MagSafe wireless charging.Face ID: Enabled by TrueDepth camera.Water Resistance: IP68.iOS: iOS 14 (upgradable).Connectivity: 5G capable, Lightning, Wi-Fi 6.Extras: Dual SIM, 4K Dolby Vision HDR recording, LiDAR Scanner for AR.",
+        img : "/assets/images/products/iphone12promax.png"
+    },
+    {
+        id : 6,
+        title : "IPhone 13 ",
+        price : 261350,
+        desc : "6.1-inch Super Retina XDR display footnote ¹.Dual-camera system for incredible low-light photos and videos.A15 Bionic with 4-core GPU for lightning-fast performance.Ceramic Shield, and water and dust resistance footnote ².5G for superfast downloads and high-quality streaming footnote ³.",
+        img : "/assets/images/products/iphone13.png"
+    },
+    {
+        id : 7,
+        title : "IPhone 13 Pro ",
+        price : 271450,
+        desc : "Display: 6.1-inch Super Retina XDR.Design: Stainless steel frame, Ceramic Shield front cover.Cameras: Triple 12MP Ultra-Wide, Wide, and Telephoto, Night mode, LiDAR Scanner.Chip: A14 Bionic.Battery: All-day battery life, MagSafe wireless charging.Face ID: Enabled by TrueDepth camera.Water Resistance: IP68.iOS: iOS 14 (upgradable).Connectivity: 5G capable, Lightning, Wi-Fi 6.Extras: Dual SIM, 4K Dolby Vision HDR recording, LiDAR Scanner for AR.",
+        img : "/assets/images/products/iphone13pro.png"
+    },
+    {
+        id : 8,
+        title : "IPhone 13 Pro Max",
+        price : 291550,
+        desc : "6.1-inch durable color-infused glass and aluminum design footnote ◊ with Ceramic Shield front.Dynamic Island bubbles up alerts and Live Activities — so you don’t miss them while you’re doing something else.48MP Main camera with 2x Telephoto captures breathtaking detail up close or far away.A16 Bionic powers computational photography and smooth gaming performance with great efficiency for all-day battery life.Connect and charge with USB-C.",
+        img : "/assets/images/products/iphone13promax.png"
+    },
+    {
+        id : 9,
+        title : "IPhone 14 ",
+        price : 311650,
+        desc : " 6.1-inch durable design footnote ¹ with Ceramic Shield and water and dust resistance.All-day battery life with up to 20 hours of video playback².Vital safety features like Roadside Assistance via satellite,³ Emergency SOS via satellite,³ and Crash Detection⁴.A Pro-level Main camera and advanced image processing for sensational shots in all kinds of light.A15 Bionic, the same superspeedy chip that’s in iPhone 13 Pro.",
+        img : "/assets/images/products/iphone14.png"
+    },
+    {
+        id : 10,
+        title : "IPhone 14 Pro ",
+        price : 321750,
+        desc : "6.1-inch durable color-infused glass and aluminum design footnote ◊ with Ceramic Shield front.Dynamic Island bubbles up alerts and Live Activities — so you don’t miss them while you’re doing something else.48MP Main camera with 2x Telephoto captures breathtaking detail up close or far away.A16 Bionic powers computational photography and smooth gaming performance with great efficiency for all-day battery life.Connect and charge with USB-C.",
+        img : "/assets/images/products/iphone14pro.png"
+    },
+    {
+        id : 11,
+        title : "IPhone 14 Pro Max",
+        price : 321850,
+        desc : "6.1-inch durable color-infused glass and aluminum design footnote ◊ with Ceramic Shield front.Dynamic Island bubbles up alerts and Live Activities — so you don’t miss them while you’re doing something else.48MP Main camera with 2x Telephoto captures breathtaking detail up close or far away.A16 Bionic powers computational photography and smooth gaming performance with great efficiency for all-day battery life.Connect and charge with USB-C.",
+        img : "/assets/images/products/iphone14promax.png"
+    },
+    {
+        id : 12,
+        title : "IPhone 15 ",
+        price : 314000,
+        desc : "6.1-inch durable color-infused glass and aluminum design footnote ◊ with Ceramic Shield front.Dynamic Island bubbles up alerts and Live Activities — so you don’t miss them while you’re doing something else.48MP Main camera with 2x Telephoto captures breathtaking detail up close or far away.A16 Bionic powers computational photography and smooth gaming performance with great efficiency for all-day battery life.Connect and charge with USB-C.",
+        img : "/assets/images/products/iphone15.png"
+    },
+    {
+        id : 13,
+        title : "IPhone 15 Pro ",
+        price : 348900,
+        desc : "6.1-inch Super Retina XDR display footnote ¹ featuring ProMotion, Always-On, and Dynamic Island.Strong and light titanium design with Action button — a fast track to your favorite feature 48MP Main camera for super-high-resolution photos and 3x Telephoto camera.A17 Pro chip delivers a massive leap in graphics performance, transforming mobile gaming.USB-C connector with USB 3 for up to 20x faster transfer speeds footnote ² and new pro workflows",
+        img : "/assets/images/products/iphone15pro.png"
+    },
+    {
+        id : 14,
+        title : "IPhone 15 Pro Max",
+        price : 398900,
+        desc : "6.1-inch durable color-infused glass and aluminum design footnote ◊ with Ceramic Shield front.Dynamic Island bubbles up alerts and Live Activities — so you don’t miss them while you’re doing something else.48MP Main camera with 2x Telephoto captures breathtaking detail up close or far away.A16 Bionic powers computational photography and smooth gaming performance with great efficiency for all-day battery life.Connect and charge with USB-C.",
+        img : "/assets/images/products/iphone15promax.png"
+    }
+]
+
+export default DATA;
